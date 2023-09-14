9e3208c4be1843bbe0b2ae05fbbedeb81477321d
kannan <133184797+rs-kannan@users.noreply.github.com>
2023-09-14 15:30:50 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,29 +6,24 @@ const sendToken = require('../utils/jwt');
 const crypto = require('crypto')
 
 //Register User - /api/v1/register
-exports.registerUser = catchAsyncError(async (req, res, next) => {
-    const {name, email, password } = req.body
-
-    let avatar;
+exports.registerUser = catchAsyncError(async(req, res, next)=> {
+    const  {name, email, password} = req.body
     
-    let BASE_URL = process.env.BACKEND_URL;
-    if(process.env.NODE_ENV === "production"){
-        BASE_URL = `${req.protocol}://${req.get('host')}`
-    }
+    let avatar;
 
     if(req.file){
-        avatar = `${BASE_URL}/uploads/user/${req.file.originalname}`
+         avatar = `${process.env.BACKEND_URL}/uploads/user/${req.file.originalname}`
     }
 
+
     const user = await User.create({
-        name,
-        email,
-        password,
-        avatar
+      name,
+      email,
+      password,
+      avatar
     });
 
-    sendToken(user, 201, res)
-
+    sendToken(user, 201, res);
 })
 
 //Login User - /api/v1/login
@@ -69,48 +64,42 @@ exports.logoutUser = (req, res, next) => {
 }
 
 //Forgot Password - /api/v1/password/forgot
-exports.forgotPassword = catchAsyncError( async (req, res, next)=>{
-    const user =  await User.findOne({email: req.body.email});
+exports.forgotpassword = catchAsyncError(async (req,res,next) => {
+    const user = await User.findOne({email : req.body.email})
 
-    if(!user) {
-        return next(new ErrorHandler('User not found with this email', 404))
+    if(!user){
+        return next(new ErrorHandler('User not found!', 404))
     }
 
     const resetToken = user.getResetToken();
-    await user.save({validateBeforeSave: false})
-    
-    let BASE_URL = process.env.FRONTEND_URL;
-    if(process.env.NODE_ENV === "production"){
-        BASE_URL = `${req.protocol}://${req.get('host')}`
-    }
+    await user.save({validateBeforeSave : false})
 
+    //creating reset URL
+    const resetUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`
 
-    //Create reset url
-    const resetUrl = `${BASE_URL}/password/reset/${resetToken}`;
+    const message = `your password reset url is as follows \n\n
+    ${resetUrl} \n\n ignore it, if you didn't request this email.`
 
-    const message = `Your password reset url is as follows \n\n 
-    ${resetUrl} \n\n If you have not requested this email, then ignore it.`;
-
-    try{
+    try {
         sendEmail({
-            email: user.email,
-            subject: "JVLcart Password Recovery",
+            email:user.email,
+            subject : "E-Cart Password Recovery",
             message
         })
 
-        res.status(200).json({
-            success: true,
-            message: `Email sent to ${user.email}`
-        })
+      res.status(200).json({
+        success:true,
+        message:`Email sent to ${user.email}`
+      })
 
-    }catch(error){
+    } catch (error) {
         user.resetPasswordToken = undefined;
         user.resetPasswordTokenExpire = undefined;
-        await user.save({validateBeforeSave: false});
-        return next(new ErrorHandler(error.message), 500)
+        await user.save({validateBeforeSave : false});
+        return next(new ErrorHandler(error.message), 500);
     }
 
-})  
+})
 
 //Reset Password - /api/v1/password/reset/:token
 exports.resetPassword = catchAsyncError( async (req, res, next) => {
@@ -165,33 +154,27 @@ exports.changePassword  = catchAsyncError(async (req, res, next) => {
  })
 
 //Update Profile - /api/v1/update
-exports.updateProfile = catchAsyncError(async (req, res, next) => {
+exports.updateProfile = catchAsyncError(async (req,res,next)=>{
     let newUserData = {
-        name: req.body.name,
-        email: req.body.email
+        name : req.body.name,
+        email : req.body.email
     }
-
+    
     let avatar;
-    let BASE_URL = process.env.BACKEND_URL;
-    if(process.env.NODE_ENV === "production"){
-        BASE_URL = `${req.protocol}://${req.get('host')}`
-    }
-
     if(req.file){
-        avatar = `${BASE_URL}/uploads/user/${req.file.originalname}`
-        newUserData = {...newUserData,avatar }
+         avatar = `${process.env.BACKEND_URL}/uploads/user/${req.file.originalname}`
+         newUserData = {...newUserData, avatar}
     }
 
-    const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
-        new: true,
+     const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+        new : true,
         runValidators: true,
     })
 
     res.status(200).json({
-        success: true,
+        success : true,
         user
     })
-
 })
 
 //Admin: Get All Users - /api/v1/admin/users
